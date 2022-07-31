5aba2409ad7b92c6386cd5938519d55444db47f1
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-31 10:47:35 +0100

    add linters

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import App from './App';import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
